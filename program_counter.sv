83a77c7778a5fceb2edbb56df046afd0d418726c
Yasamankhe <nasayasi71@gmail.com>
2026-09-04 17:13:35 -0700

    Update program_counter.sv

--- program_counter.sv
+++ program_counter.sv
@@ -1,1 +1,28 @@
+module program_counter(
+    clk,
+    reset,
+    pc
+);
 
+input clk;
+input reset;
+
+output logic [7:0] pc;
+
+
+// Program counter:
+// reset = 1  -> go back to instruction 0
+// reset = 0  -> move to the next instruction every clock
+
+always_ff @(posedge clk) begin
+
+    if (reset)
+        pc <= 8'b00000000;
+
+    else
+        pc <= pc + 1'b1;
+
+end
+
+
+endmodule
